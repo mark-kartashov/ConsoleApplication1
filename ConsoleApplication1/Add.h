167fda15df6cca97880cabc6f10870ed9ea640b8
Kartashov Mark <40755671+mark-kartashov@users.noreply.github.com>
2020-06-10 17:56:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Add.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Определите собственное пространство имён для хранения констант
+namespace constants
+{
+    const double my_gravity(9.8);
+}
+#endif
---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include "Add.h"
+
+using namespace std;
+
+double getHeight() {
+    cout << "Enter the height of the tower: " << "\n";
+    double x;
+    cin >> x;
+    return x;
+}
+
+double getBallHeigh(double towerHeight, int timing) {
+     double BallHeigh = towerHeight - ((constants::my_gravity * timing * timing) / 2);
+     return BallHeigh;
+}
+
+void printResult(double towerHeight, int timing) {
+    double x = getBallHeigh(towerHeight, timing);
+    if (x > 0.0) {
+        cout << "In " << timing << "Ball Heigh is" << x << "\n";
+    }
+    else {
+        cout << "Ball on flour" << "\n";
+    }
+}
+
+int main()
+{
+    
+    const double towerHeight{ getHeight() };
+    printResult(towerHeight, 0);
+    printResult(towerHeight, 1);
+    printResult(towerHeight, 2);
+    printResult(towerHeight, 3);
+    printResult(towerHeight, 4);
+    printResult(towerHeight, 5);
+
+    return 0;
+}
